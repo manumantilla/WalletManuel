00d35f2a32431dca97e8b2297ac0253b256250ce
manumantilla <100888893+manumantilla@users.noreply.github.com>
2023-09-15 23:26:19 -0500

    Initial commit

---
+++ app/src/main/java/com/walletManuelDelgadoMantilla/Account.java
@@ -0,0 +1,51 @@
+package com.walletManuelDelgadoMantilla;
+
+
+public class Account {
+
+    public Account(String name, String typeAccount, Double currentValue, String imageUrl) {
+        this.name = name;
+        this.typeAccount = typeAccount;
+        this.currentValue = currentValue;
+        this.imageUrl = imageUrl;
+    }
+
+    private String name;
+    private String typeAccount;
+    private Double currentValue;
+    private String imageUrl;
+
+    public String getInageUrl() {
+        return imageUrl;
+    }
+
+    public void setInageUrl(String inageUrl) {
+        this.imageUrl = inageUrl;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTypeAccount() {
+        return typeAccount;
+    }
+
+    public void setTypeAccount(String typeAccount) {
+        this.typeAccount = typeAccount;
+    }
+
+    public Double getCurrentValue() {
+        return currentValue;
+    }
+
+    public void setCurrentValue(Double currentValue) {
+        this.currentValue = currentValue;
+    }
+}
---
+++ app/src/main/java/com/walletManuelDelgadoMantilla/ListAccountActivity.java
@@ -0,0 +1,33 @@
+package com.walletManuelDelgadoMantilla;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class ListAccountActivity extends AppCompatActivity {
+
+    private ArrayList<Account> listAccountRV = new ArrayList<>();
+    private RecyclerView rvListAccount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_account);
+        loadFakeData();
+        rvListAccount = findViewById(R.id.rv_account);
+        AccountAdapter myAdaptader = new AccountAdapter(listAccountRV);
+        rvListAccount.setAdapter(myAdaptader);
+        rvListAccount.setLayoutManager(new LinearLayoutManager(ListAccountActivity.this));
+    }
+    private void loadFakeData(){
+        Account myAccount1 = new Account("BANCOLOMBIA","Cuenta Corriente",3321.38,"https://i.pinimg.com/originals/b8/cd/c1/b8cdc1ad498fe080bc21bb5a03c24f83.png");
+        Account myAccount2 = new Account("DAVIVIENDA","Cuenta Ahorros",871278.24,"https://pbs.twimg.com/profile_images/1002552048620134400/qZ1XCo_9_400x400.jpg");
+        Account myAccount3 = new Account("OCCIDENTE","Tarjeta Credito",123313.92,"https://seeklogo.com/images/B/Banco_de_Bogota-logo-609A0072EA-seeklogo.com.png");
+        listAccountRV.add(myAccount1);
+        listAccountRV.add(myAccount2);
+        listAccountRV.add(myAccount3);
+    }
+}
